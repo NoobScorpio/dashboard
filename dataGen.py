126bccf67a38234f000f477575bd2509b9549b18
Noob Scorpio <aliadam7867@gmail.com>
2021-01-15 10:24:47 +0500

    GEN DATA

--- dataGen.py
+++ dataGen.py
@@ -7,66 +7,77 @@ from random import randrange
 from datetime import timedelta
 from datetime import datetime
 import pandas as pd
-d1 = datetime.strptime('1/1/2005 1:30 PM', '%m/%d/%Y %I:%M %p')
-d2 = datetime.strptime('1/1/2021 4:50 AM', '%m/%d/%Y %I:%M %p')
-
-invoice=0
-invoiceNo=[]
-itemNo=[]
-items=dict()
-cost=[]
-qnty=[]
-cust=1
-discount=[]
-dates=[]
-custNo=[]
-price=[]
-
-def random_date(start, end):
-    """
-    This function will return a random datetime between two datetime 
-    objects.
-    """
-    delta = end - start
-    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
-    random_second = randrange(int_delta)
-    return start + timedelta(seconds=random_second)
-
-date=random_date(d1,d2)
-
-for i in range(1,6331):
-    items[i]=uniform(0.5,65160.5)
 
 
-print('Making Data Set')
-for i in range(0,20000000):
-    if i%25==0:
-        invoice=invoice+1
-        date=random_date(d1,d2)
-        cust=randint(1,1500)
-    invoiceNo.append(invoice)
-    no=randint(1,6330)
-    itemNo.append(no)
-    cost.append(items[no])
-    dates.append(date)
-    custNo.append(cust)
-    price.append(cost[-1]+uniform(2.5,15000.5))
-    discount.append(uniform(0,1500))
-    qnty.append(randint(1,3300))
-    if i%1000000==0:
-        print('Million REACHER -----------------')
 
-df_details={
-    'InvoiceNo':invoiceNo,
-    'ItemNo':itemNo,
-    'CustomerNo':custNo,
-    'Price':price,
-    'Cost':cost,
-    'Qnty':qnty,
-    'Discount':discount,
-    'InvoiceDate':dates,
-    }
-df_details = pd.DataFrame(df_details, columns = ['InvoiceNo','ItemNo','CustomerNo','Price','Cost','Qnty','Discount','InvoiceDate'])
-df_details.to_csv('genData.csv')
-df=pd.read_csv('genData.csv')
-print(df.head())
+df = pd.read_csv('data/genData.csv')
+df['Year']=df['InvoiceDate'].dt.year
+df['Month']=df['InvoiceDate'].dt.month
+df['Day']=df['InvoiceDate'].dt.day
+df['TotalCost']=df['Cost']*df['Qnty']
+df['TotalPrice']=df['Price']*df['Qnty']
+df['TotalProfit']=df['TotalPrice']-df['TotalCost']-df['Discount']
+pd.to_csv('data/genData.csv')
+# d1 = datetime.strptime('1/1/2005 1:30 PM', '%m/%d/%Y %I:%M %p')
+# d2 = datetime.strptime('1/1/2021 4:50 AM', '%m/%d/%Y %I:%M %p')
+
+# invoice=0
+# invoiceNo=[]
+# itemNo=[]
+# items=dict()
+# cost=[]
+# qnty=[]
+# cust=1
+# discount=[]
+# dates=[]
+# custNo=[]
+# price=[]
+
+# def random_date(start, end):
+#     """
+#     This function will return a random datetime between two datetime 
+#     objects.
+#     """
+#     delta = end - start
+#     int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+#     random_second = randrange(int_delta)
+#     return start + timedelta(seconds=random_second)
+
+# date=random_date(d1,d2)
+
+# for i in range(1,6331):
+#     items[i]=uniform(0.5,65160.5)
+
+
+# print('Making Data Set')
+# for i in range(0,20000000):
+#     if i%25==0:
+#         invoice=invoice+1
+#         date=random_date(d1,d2)
+#         cust=randint(1,1500)
+#     invoiceNo.append(invoice)
+#     no=randint(1,6330)
+#     itemNo.append(no)
+#     cost.append(items[no])
+#     dates.append(date)
+#     custNo.append(cust)
+#     price.append(cost[-1]+uniform(2.5,15000.5))
+#     discount.append(uniform(0,1500))
+#     qnty.append(randint(1,3300))
+#     if i%1000000==0:
+#         print('Million REACHER -----------------')
+
+# df_details={
+#     'InvoiceNo':invoiceNo,
+#     'ItemNo':itemNo,
+#     'CustomerNo':custNo,
+#     'Price':price,
+#     'Cost':cost,
+#     'Qnty':qnty,
+#     'Discount':discount,
+#     'InvoiceDate':dates,
+#     }
+# df_details = pd.DataFrame(df_details, columns = ['InvoiceNo','ItemNo','CustomerNo','Price','Cost','Qnty','Discount','InvoiceDate'])
+# df_details.to_csv('genData.csv')
+# df=pd.read_csv('genData.csv')
+# print(df.head())
