5501dc85843215c6a23cf07889900f5ed92a8e3d
Noob Scorpio <aliadam7867@gmail.com>
2021-01-15 10:10:08 +0500

    MAIN

--- main.py
+++ main.py
@@ -416,5 +416,5 @@ def display_page(pathname):
 
 
 if __name__ == '__main__':
-    app.run_server(port='5000', debug=False)
+    app.run_server(host='0.0.0.0',port='5000', debug=False)
 # host='0.0.0.0',
